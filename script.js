2727bf24e130c1120200fd58bf524a8028c9e09d
Valtemir <valmartinsfilho@gmail.com>
2024-11-13 17:40:36 -0300

    funções hover e alterações de cores

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function showLinks() {
   const links = document.getElementById('links');
   links.classList.toggle('hidden');
 }
+function selectCategory(category, btn) {
+  selectedCategory = category;
+  // Remove a classe 'selected' de todos os botões de categoria
+  document.querySelectorAll('.submit-btn').forEach(button => button.classList.remove('selected'));
+  // Adiciona a classe 'selected' ao botão clicado
+  btn.classList.add('selected');
+  document.getElementById('btnNext1').disabled = false;
+}
+
+function selectActivity(activityFactor, btn) {
+  selectedActivity = activityFactor;
+  // Remove a classe 'selected' de todos os botões de atividade
+  document.querySelectorAll('.submit-btn').forEach(button => button.classList.remove('selected'));
+  // Adiciona a classe 'selected' ao botão clicado
+  btn.classList.add('selected');
+  document.getElementById('btnNext3').disabled = false;
+}
